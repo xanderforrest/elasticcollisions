f725278afa2fa0c83637b66f4e57680a23b69d89
xanderforrest <62808098+xanderforrest@users.noreply.github.com>
2020-03-28 18:19:30 +0000

    Relatively working

--- main.py
+++ main.py
@@ -24,7 +24,11 @@ screen.fill((255, 255, 255))
 
 def apply_collision(ball_a, ball_b):
     normal_vector = ball_b.get_position() - ball_a.get_position()
-    nu_vector = normal_vector / np.linalg.norm(normal_vector)
+    try:
+        nu_vector = normal_vector / np.linalg.norm(normal_vector)
+    except:
+        print("Division by 0 Error, cancelling collision")
+        return False
     tan_vector = np.array([nu_vector[1]*-1, nu_vector[0]])
 
     v1n = np.dot(nu_vector, ball_a.get_velocity())
@@ -53,14 +57,20 @@ class Ball(pygame.sprite.Sprite):
         super(Ball, self).__init__()
         self.surf = BALL
         self.surf.set_colorkey((0, 0, 0), RLEACCEL)
-        self.rect = self.surf.get_rect()
-        self.v = np.array([random.randint(1, 3), 1])
-        self.mass = random.randint(1, 3)
+        self.rect = self.surf.get_rect(
+            center = (
+                random.randint(0, SCREEN_WIDTH),
+                random.randint(0, SCREEN_HEIGHT)
+                ))
+        self.v = np.array([random.randint(-6, 6), random.randint(-6, 6)])
+        self.mass = random.randint(20, 100)
 
     def update_velocity(self, velocity):
-        #print(f"MY OLD VELOCITY WAS {self.v}")
+        print(f"MY OLD VELOCITY WAS {self.v}")
         self.v = velocity
-        #print(f"MY NEW VELOCITY IS {self.v}")
+        print(f"MY NEW VELOCITY IS {self.v}")
+        print(f"MY MASS IS {self.mass}")
+        self.rect.move_ip(1.5*self.v[0], 1.5*self.v[1])
 
     def update(self):
         self.rect.move_ip(self.v[0], self.v[1])
@@ -87,8 +97,13 @@ all_balls = pygame.sprite.Group()
 ball = Ball()
 all_balls.add(ball)
 
+ALLOWCOLLISION = pygame.USEREVENT + 1
+pygame.time.set_timer(ALLOWCOLLISION, 1000)
+clock = pygame.time.Clock()
+collided = []
 running = True
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -98,6 +113,8 @@ while running:
                 all_balls.add(new_ball)
         elif event.type == QUIT:
             running = False
+        elif event.type == ALLOWCOLLISION:
+            collided = []
 
     all_balls.update()
     screen.fill((255, 255, 255))
@@ -116,9 +133,18 @@ while running:
 
     for entity in all_balls:
         all_balls.remove(entity)
+        for ball in collided:
+            try:
+                all_balls.remove(ball)
+            except:
+                pass
         ball_b = pygame.sprite.spritecollideany(entity, all_balls)
-        if ball_b:
+        if ball_b and ball_b not in collided:
             #print("There was a collision")
             apply_collision(entity, ball_b)
-        all_balls.add(entity)
-
+            collided.append(ball_b)
+        if entity not in all_balls:
+            all_balls.add(entity)
+        for ball in collided:
+            if ball not in all_balls:
+                all_balls.add(ball)
